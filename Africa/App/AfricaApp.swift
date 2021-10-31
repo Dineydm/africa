394d766d7c5754cb63d5d4b285fad536a72ba713
Diney <dineydm@gmail.com>
2021-10-31 18:56:25 -0300

    first commit

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Valdiney Menezes on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
